a6e91ac6321cd8fc6e3313cf09fe436895f5c978
SharkTal <liutao.gao@gmail.com>
2022-09-05 22:01:32 +0300

    flatlist shopping cart

--- shoppinglist/App.js
+++ shoppinglist/App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+/* SBL: 1. input- var: item , item array- cart
+         2, functions - add, clear;
+         3. display */
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [cart, setCart] = useState([]);
+
+  const add = () => {
+    setCart([...cart, { key: text }]);
+    setText('');
+  }
+
+  const clear = () => {
+    setCart([]);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+      <Button
+        style={styles.button}
+        onPress={add}
+        title="Add"
+      />
+      <Button
+        style={styles.button}
+        onPress={clear}
+        title="Clear"
+      />
+
+
+      <FlatList
+        style={styles.list}
+        data={cart}
+        renderItem={
+          ({ item }) => <Text> {item.key} </Text>
+        }
+      />
+
     </View>
   );
 }
@@ -17,4 +57,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
